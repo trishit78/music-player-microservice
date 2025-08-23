dfe74a433ef3a4a54dcfa88804d2a6205953b9fc
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-08-23 20:36:45 +0530

    added model users

---
+++ src/model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document } from "mongoose";
+
+interface IUser extends Document {
+    name: string;   
+    email: string;
+    password: string;
+    role: string;
+    playlist: string[];
+
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true , default: "user"},  
+    playlist: [{
+        type:String,
+        required:true
+    }]
+},{timestamps:true});
+
+export const User =  mongoose.model<IUser>("User", userSchema);
