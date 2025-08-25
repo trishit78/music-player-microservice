be0f3af089f9e3db5bd27bd7aff7b252fe40ee07
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-08-25 23:18:58 +0530

    added all the api of admin service

--- admin service/src/controller.ts
+++ admin service/src/controller.ts
@@ -56,3 +56,178 @@ export const addAlbum = TryCatch(async (req:AuthRequest, res) => {
 
     
 });
+
+
+
+export const addSong = TryCatch(async (req:AuthRequest, res) => {
+    console.log("this is add song route");
+
+
+    if(req.user?.role !== "admin"){
+     res.status(403).json({
+         success: false,
+         message: "Forbidden, you are not admin"
+     });
+     return;
+    }
+ 
+    const {title, descriptions, album} = req.body;
+
+    const albumId = Number(album);
+const isAlbum = await sql`SELECT id FROM albums WHERE id = ${albumId}`;
+
+    // const isAlbum = await sql `SELECT id FROM albums WHERE id = ${album}`;
+    if(isAlbum.length === 0){
+     res.status(400).json({
+            success: false,
+            message: "Album not found"
+        });
+        return;
+    }
+
+    console.log("The req file is",req.file)
+    const file = req.file;
+    if(!file){
+     res.status(400).json({
+         success: false,
+         message: "File is required"
+     });
+     return;
+    }
+ 
+
+
+
+    const fileBuffer = getBuffer(file);
+    if(!fileBuffer || !fileBuffer.content){
+     res.status(500).json({
+         success: false,
+         message: "failed to generate file buffer"
+     });
+     return;
+    }
+ 
+    const cloud = await cloudinary.uploader.upload(fileBuffer.content,{ folder: "songs",resource_type:"video" });
+    
+    const result = await sql`
+    INSERT INTO songs (title, descriptions, audio, album_id)  
+     VALUES (${title}, ${descriptions}, ${cloud.secure_url}, ${album}) RETURNING *;
+     `;
+
+
+     res.json({
+         message: "Song added successfully",
+         song: result[0],   
+         success: true
+     });
+ 
+ });
+
+
+ export const addThumbnail = TryCatch(async (req:AuthRequest, res) => {
+    if(req.user?.role !== "admin"){
+     res.status(403).json({
+         success: false,
+         message: "Forbidden, you are not admin"
+     });
+     return;
+    }
+ 
+   const song = await sql`SELECT id FROM songs WHERE id = ${Number(req.params.id)}`;
+    
+   if(song.length === 0){
+        res.status(400).json({
+                success: false,
+                message: "Song not found"
+            });
+            return;
+    }
+
+ 
+    const file = req.file;
+    if(!file){
+     res.status(400).json({
+         success: false,
+         message: "File is required"
+     });
+     return;
+    }
+ 
+    const fileBuffer = getBuffer(file);
+    if(!fileBuffer || !fileBuffer.content){
+     res.status(500).json({
+         success: false,
+         message: "failed to generate file buffer"
+     });
+     return;
+    }
+    const cloud = await cloudinary.uploader.upload(fileBuffer.content,{ folder: "songs",resource_type:"image" });
+    
+    const result = await sql`
+    UPDATE songs SET thumbnail = ${cloud.secure_url} WHERE id = ${req.params.id} RETURNING *;
+     `;
+     res.json({
+         message: "Thumbnail added successfully",
+         song: result[0],   
+         success: true
+     });
+ 
+     
+ });
+
+ export const deleteAlbum = TryCatch(async (req:AuthRequest, res) => {
+    if(req.user?.role !== "admin"){
+     res.status(403).json({
+         success: false,
+         message: "Forbidden, you are not admin"
+     });
+     return;
+    }
+ const isAlbum = await sql`SELECT * FROM albums WHERE id = ${Number(req.params.id)}`;
+    if(isAlbum.length === 0){
+     res.status(400).json({ 
+            success: false,
+            message: "Album not found"
+        });
+        return;
+    };
+
+
+    await sql`DELETE FROM songs WHERE album_id = ${Number(req.params.id)}`;
+
+    await sql`DELETE FROM albums WHERE id = ${Number(req.params.id)}`;
+    
+    res.json({
+        success: true,
+        message: "Album deleted successfully"
+    });
+    
+ });
+
+
+ export const deleteSong = TryCatch(async (req:AuthRequest, res) => {
+    if(req.user?.role !== "admin"){
+     res.status(403).json({
+         success: false,
+         message: "Forbidden, you are not admin"
+     });
+     return;
+    }       
+
+ const isSong = await sql`SELECT * FROM albums WHERE id = ${Number(req.params.id)}`;
+    if(isSong.length === 0){
+     res.status(400).json({ 
+            success: false,
+            message: "song not found"
+        });
+        return;
+    };
+
+    await sql`DELETE FROM songs WHERE id = ${Number(req.params.id)}`;
+    res.json({
+        success: true,
+        message: "Song deleted successfully"
+        
+    });
+});
+
--- admin service/src/route.ts
+++ admin service/src/route.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { isAuth } from './middlware.js';
-import { addAlbum } from './controller.js';
+import { addAlbum, addSong, addThumbnail, deleteAlbum, deleteSong } from './controller.js';
 import uploadFile  from './uploadFile.js';
 
 
 const router = express.Router();
 router.post('/album/new',isAuth,uploadFile,addAlbum); 
+router.post('/song/new',isAuth,uploadFile,addSong); 
+router.post('/song/:id',isAuth,uploadFile,addThumbnail); 
+router.delete('/album/:id',isAuth,deleteAlbum); 
+router.delete('/song/:id',isAuth,deleteSong); 
 
 export default router;
