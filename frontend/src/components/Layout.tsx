795f021984c9541bf0eb1f767b37bc51c68e1af3
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-09-05 20:40:05 +0530

    sidebar ui done

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,26 @@
+
+import Sidebar from "./Sidebar"
+import Navbar from "./Navbar"
+import type { ReactNode } from "react"
+import Player from "./Player"
+
+interface LayoutProps{
+    children:ReactNode
+}
+
+const Layout:React.FC<LayoutProps> = ({children}) =>{
+    return (
+        <div className="h-screen">
+            <div className="h-[90%] flex">
+                <Sidebar />
+                <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+                <Navbar />
+                {children}
+                </div>
+            </div>
+            <Player />
+        </div>
+    )
+}
+
+export default Layout;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout"
+
+const Home = () => {
+  return (
+    <div>
+        <Layout>Home</Layout>
+    </div>
+  )
+}
+
+export default Home
